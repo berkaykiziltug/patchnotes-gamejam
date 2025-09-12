9af231c6fb725f8140db17a8eb0d36597200c58c
berkaykiziltug <berkay.kzt@gmail.com>
2025-09-12 13:10:35 +0300

    add random jump spins

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,7 +8,7 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Settings")]
     [SerializeField] private float forwardSpeed = 5f;
     [SerializeField] private float jumpHeight = 2f;
-    [SerializeField] private float jumpDuration = 0.5f; 
+    [SerializeField] private float jumpDuration = 0.5f; // time in seconds for the jump
     [SerializeField] float jumpPower;
     private bool canJump;
     [SerializeField]private bool isGrounded;
@@ -27,7 +28,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.3f; 
+        Vector3 rayOrigin = transform.position + Vector3.up * 0.3f; //
         isGrounded = Physics.Raycast(rayOrigin, Vector3.down, groundCheckDistance, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -40,20 +41,30 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Jump()
-    {
-        Vector3 targetPosition = transform.position + transform.forward * jumpForwardDistance;
-        transform.DOJump(targetPosition, jumpPower, 1, jumpDuration)
-                 .SetEase(Ease.Linear)
-                 .OnStart(() =>
-                 {
-                     //Rotating during jump
-                     transform.DOLocalRotate(new Vector3(360f, 0f, 0f), jumpDuration, RotateMode.LocalAxisAdd)
-                              .SetEase(Ease.Linear);
-                 }).OnComplete(() =>
-                 {
-                     canJump = true;
-                 });
-    }
+{
+    Vector3 targetPosition = transform.position + transform.forward * jumpForwardDistance;
+
+    //Number of spins player can do. 4 maybe a bit too much. Can reduce it to 3 or 2.
+    int spins = UnityEngine.Random.Range(1, 4); 
+
+    
+    float rotationDuration = jumpDuration; 
+
+    transform.DOJump(targetPosition, jumpPower, 1, jumpDuration)
+             .SetEase(Ease.Linear)
+             .OnStart(() =>
+             {
+                 
+                 transform.DOLocalRotate(new Vector3(360f * spins, 0f, 0f), rotationDuration, RotateMode.FastBeyond360)
+                          .SetEase(Ease.Linear);
+             })
+             .OnComplete(() =>
+             {
+                 
+                 transform.rotation = Quaternion.identity;
+                 canJump = true;
+             });
+}
     
 
         void OnDrawGizmos()
