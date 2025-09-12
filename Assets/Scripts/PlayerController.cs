0ebdc5dc99ce2e08b3586e02426a1dd150915e29
berkaykiziltug <berkay.kzt@gmail.com>
2025-09-12 17:19:59 +0300

    switch to physics movement and add stamina costs with stamina slider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,73 +1,138 @@
+using System.Collections;
 using DG.Tweening;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
     [Header("Movement Settings")]
     [SerializeField] private float forwardSpeed = 5f;
     [SerializeField] private float jumpHeight = 2f;
-    [SerializeField] private float jumpDuration = 0.5f; // time in seconds for the jump
+    [SerializeField] private float jumpDuration = 0.5f;
     [SerializeField] float jumpPower;
+    private Rigidbody rb;
     private bool canJump;
-    [SerializeField]private bool isGrounded;
-    
+    [SerializeField] private bool isGrounded;
+
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundCheckDistance;
 
-    private float jumpForwardDistance = 4;
- 
- 
+    [SerializeField] private float stamina = 250;
+    [SerializeField] private Slider staminaSlider;
+    [SerializeField] private float staminaRegenRate = 30f;
+
+    private bool hasStamina => stamina > 20;
+
+
+
+    private bool isRegenerating;
+
+    //  private SphereCollider groundDetectionCollider;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        staminaSlider.minValue = 0f;
+        staminaSlider.maxValue = 250f;
+        staminaSlider.value = stamina;
+        // groundDetectionCollider = GetComponent<SphereCollider>();
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.3f; //
-        isGrounded = Physics.Raycast(rayOrigin, Vector3.down, groundCheckDistance, groundLayer);
+        // Vector3 rayOrigin = transform.position + Vector3.up * 0.3f; //
+        // isGrounded = Physics.Raycast(rayOrigin, Vector3.down, groundCheckDistance, groundLayer);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && stamina >= 20f && !isRegenerating)
         {
             Debug.Log("Jumping");
-            canJump = false;
-            Jump();
+            canJump = true;
         }
-        
+        if (stamina <= 20f && !isRegenerating)
+        {
+        isRegenerating = true;
+        canJump = false;
+        StartCoroutine(RegenerateStamina());
+        }
+
+       
+    }
+    private IEnumerator RegenerateStamina()
+    {
+        yield return new WaitForSeconds(5f);
+
+        while (stamina < staminaSlider.maxValue)
+        {
+            stamina += staminaRegenRate * Time.deltaTime;
+            stamina = Mathf.Min(stamina, staminaSlider.maxValue);
+            UpdateStaminaSlider(stamina);
+            yield return null;
+        }
+        isRegenerating = false;
+        // canJump = true;
+    }
+
+
+    void FixedUpdate()
+    {
+        if (canJump)
+        {
+            Jump();
+            AddRotation();
+            AddForwardForce();
+        }
+        canJump = false;
     }
 
     private void Jump()
-{
-    Vector3 targetPosition = transform.position + transform.forward * jumpForwardDistance;
+    {
+        float randomJumpForce = UnityEngine.Random.Range(4, 15);
+        float staminaCost = Mathf.Lerp(4, 15, (randomJumpForce - 4) / (15 - 4));
+        Debug.Log($"Stamina cost is {staminaCost}");
+        rb.AddForce(new Vector3(0, randomJumpForce, 0), ForceMode.Impulse);
+        canJump = false;
+        isGrounded = false;
+        stamina = Mathf.Max(0, stamina - staminaCost);
+        UpdateStaminaSlider(stamina);
+    }
 
-    //Number of spins player can do. 4 maybe a bit too much. Can reduce it to 3 or 2.
-    int spins = UnityEngine.Random.Range(1, 4); 
+    private void AddRotation()
+    {
+        float randomRotateForce = UnityEngine.Random.Range(1, 3);
+        rb.AddTorque(new Vector3(randomRotateForce, 0, 0), ForceMode.Impulse);
+    }
+    private void AddForwardForce()
+    {
+        float randomForwardForce = UnityEngine.Random.Range(1, 8);
+        rb.AddForce(new Vector3(0, 0, randomForwardForce), ForceMode.Impulse);
+    }
 
-    
-    float rotationDuration = jumpDuration; 
 
-    transform.DOJump(targetPosition, jumpPower, 1, jumpDuration)
-             .SetEase(Ease.Linear)
-             .OnStart(() =>
-             {
-                 
-                 transform.DOLocalRotate(new Vector3(360f * spins, 0f, 0f), rotationDuration, RotateMode.FastBeyond360)
-                          .SetEase(Ease.Linear);
-             })
-             .OnComplete(() =>
-             {
-                 
-                 transform.rotation = Quaternion.identity;
-                 canJump = true;
-             });
-}
-    
-
-        void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
@@ -75,5 +140,10 @@ public class PlayerController : MonoBehaviour
         Vector3 rayEnd = rayOrigin + Vector3.down * groundCheckDistance;
 
         Gizmos.DrawLine(rayOrigin, rayEnd);
+    }
+
+    private void UpdateStaminaSlider(float value)
+    {
+        staminaSlider.DOValue(value, .25f);
     }
 }
