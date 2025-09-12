fb2d7caeda1375980d55be7f8c7fcf3bd1c91a84
berkaykiziltug <berkay.kzt@gmail.com>
2025-09-12 12:52:07 +0300

    add dotween jump without physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,44 +1,68 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    [Header("Movement Settings")]
+    [SerializeField] private float forwardSpeed = 5f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float jumpDuration = 0.5f; 
+    [SerializeField] float jumpPower;
     private bool canJump;
-    private bool isGrounded;
-    private Rigidbody rb;
+    [SerializeField]private bool isGrounded;
+    
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float forwardForce;
     [SerializeField] private float groundCheckDistance;
+
+    private float jumpForwardDistance = 4;
+ 
+ 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        canJump = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance , groundLayer);
+        Vector3 rayOrigin = transform.position + Vector3.up * 0.3f; 
+        isGrounded = Physics.Raycast(rayOrigin, Vector3.down, groundCheckDistance, groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            canJump = true;
-        }
-    }
-    void FixedUpdate()
-    {
-        if (canJump)
-        {
-             Vector3 jumpVector = Vector3.up * jumpForce + transform.forward * forwardForce;
-            rb.AddForce(jumpVector, ForceMode.Impulse);
+            Debug.Log("Jumping");
             canJump = false;
+            Jump();
         }
+        
     }
 
-    void OnDrawGizmos()
+    private void Jump()
+    {
+        Vector3 targetPosition = transform.position + transform.forward * jumpForwardDistance;
+        transform.DOJump(targetPosition, jumpPower, 1, jumpDuration)
+                 .SetEase(Ease.Linear)
+                 .OnStart(() =>
+                 {
+                     //Rotating during jump
+                     transform.DOLocalRotate(new Vector3(360f, 0f, 0f), jumpDuration, RotateMode.LocalAxisAdd)
+                              .SetEase(Ease.Linear);
+                 }).OnComplete(() =>
+                 {
+                     canJump = true;
+                 });
+    }
+    
+
+        void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundCheckDistance);
+
+        Vector3 rayOrigin = transform.position + Vector3.up * 0.3f;
+        Vector3 rayEnd = rayOrigin + Vector3.down * groundCheckDistance;
+
+        Gizmos.DrawLine(rayOrigin, rayEnd);
     }
 }
